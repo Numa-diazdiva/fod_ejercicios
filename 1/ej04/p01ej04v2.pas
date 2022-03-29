2be113415b81949909ff54b5b91c98b7f167afbe
Numa-diazdiva <manuel.diazd@hotmail.com>
2022-03-29 02:17:24 -0300

    Añado ejs 4 y 5; al 5 le falta funcionar, tira runtime error en la lectura del txt.

---
+++ 1/ej04/p01ej04v2.pas
@@ -0,0 +1,319 @@
+{
+* 4. Agregar al menú del programa del ejercicio 3, opciones para:
+a.
+Añadir una o más empleados al final del archivo con sus datos ingresados por
+teclado.
+b. Modificar edad a una o más empleados.
+c. Exportar el contenido del archivo a un archivo de texto llamado “todos_empleados.txt”.
+d. Exportar a un archivo de texto llamado: “faltaDNIEmpleado.txt”, los empleados que no tengan cargado el DNI (DNI en 00).
+NOTA: Las búsquedas deben realizarse por número de empleado.
+* }
+
+
+
+program fod_p01ej04;
+
+
+Type
+	cadena20 = string[20];
+	empleado = record
+		apellido: cadena20;
+		nombre: cadena20;
+		nroEmpleado: integer;
+		edad: integer;
+		dni: integer;
+	end;
+	
+	empleadoDetalle = record
+		nroEmpleado: integer;
+		edad: integer;
+	end;
+	
+	archivo = file of empleado;
+	archDetalle = file of empleadoDetalle;
+
+procedure LeerEmpleado(var e:empleado);
+	begin
+		writeln('---> Ingrese los datos del empleado, para finalizar ingrese el apellido "fin".');
+		writeln('Apellido: ');
+		readln(e.apellido);
+		if (e.apellido <> 'fin') then begin
+			write('Ingrese el nombre: '); readln(e.nombre);
+			write('Ingrese el nro. de empleado: '); readln(e.nroEmpleado);
+			write('Ingrese su edad: '); readln(e.edad);
+			write('Ingrese su dni: '); readln(e.dni);
+		end;
+	end;
+
+
+procedure CrearArchivo(var arch:archivo);
+	var
+		e:empleado;
+	begin
+		rewrite(arch);
+		writeln('-----------Escritura de archivo---------');
+		LeerEmpleado(e);
+		while(e.apellido <> 'fin') do begin
+			write(arch,e);
+			LeerEmpleado(e);	
+		end;
+		
+		close(arch);
+		writeln('--------------EscrituraFinalizada-------------');
+		
+	end;
+
+{------------------------------- impresión ---------------------------------}
+
+procedure Imprimir(e: empleado);
+	begin
+		writeln('---------------Empleado nro: ', e.nroEmpleado);
+		writeln('Nombre: ',e.nombre);
+		writeln('Apellido: ', e.apellido);
+		writeln('Edad: ', e.edad);
+		writeln('DNI: ', e.dni);
+	end;
+
+{	ver cómo evitar repetir tanto código	}
+procedure ImprimirNombreAp(var arch:archivo);
+	var
+		e: empleado;
+		o: string[1];
+		datoBuscar: cadena20;
+	begin
+		reset(arch);
+		
+		writeln('-----------------Impresión por nombre o apellido-----------------');
+		writeln('Ingrese "n" o "a" según qué desee buscar, o cualquier otra cosa para salir: ');
+		readln(o);
+		
+		if (o = 'a') then begin
+			write('Ingrese el apellido a buscar: '); readln(datoBuscar);
+			
+			while (not eof(arch)) do begin
+				read(arch, e);
+				if (e.apellido  = datoBuscar) then begin
+					Imprimir(e);
+				end;
+			end;
+		end
+		else begin
+			if (o = 'n') then begin
+				write('Ingrese el nombre a buscar: '); readln(datoBuscar);
+				
+				while (not eof(arch)) do begin
+					read(arch, e);
+					if (e.nombre  = datoBuscar) then begin
+						Imprimir(e);
+					end;
+				end;
+			end
+			else begin
+				writeln('Saliendo...');
+			end;
+		end;
+		
+		close(arch);
+	end;
+					
+procedure ImprimirTodo(var arch:archivo);
+	var
+		e:empleado;
+	begin
+		reset(arch);
+		writeln('----------------- Empleados -----------------');
+		while (not eof(arch)) do begin
+			read(arch,e);
+			Imprimir(e);
+		end;
+		close(arch);
+	end;
+
+procedure ImprimirMayores(var arch:archivo);
+	var
+		e:empleado;
+	begin
+		reset(arch);
+		writeln('------------------ Empleados mayores de 70 anios -----------------');
+		while (not eof(arch)) do begin
+			read(arch,e);
+			if(e.edad > 70) then
+				Imprimir(e);
+		end;
+		close(arch);
+	end;
+
+
+{---------------------------------------- PUNTO 4 -------------------------------}
+
+procedure AgregarAlFinal(var aL:archivo);
+	var
+		e:empleado;
+	begin
+		{Abrimos el archivo y nos paramos sobre el final}
+		reset(aL);
+		seek(aL, filesize(aL));
+		writeln('------------AGREGAR AL FINAL-----------');
+		LeerEmpleado(e);
+		while (e.apellido <> 'fin') do begin
+			write(aL,e);
+			LeerEmpleado(e);
+		end;
+		close(aL);
+	end;
+	
+
+
+procedure CargarDetalles(var aD:archDetalle);
+	var
+		eD:empleadoDetalle;
+	begin
+		assign(aD, 'detalle');
+		rewrite(aD);
+		writeln('-----------------ACTUALIZACIóN DE EDAD ----------------');
+		writeln('Ingrese el número de empleado cuya edad quiere actualizar. Para salir ingrese 0: ');
+		readln(eD.nroEmpleado);
+		while (eD.nroEmpleado <> 0) do begin
+			write('Ingrese su edad: ');
+			readln(eD.edad);
+			write(aD,eD);
+			
+			write('Ingrese el número de empleado cuya edad quiere actualizar. Para salir ingrese 0: ');
+			readln(eD.nroEmpleado);
+		end;
+		
+		close(aD);	
+	end;
+	
+	
+	
+procedure ActualizarMaestroDetalle(var aM:archivo);
+	var
+		aD:archDetalle;
+		empM:empleado; empD:empleadoDetalle;
+	begin
+		CargarDetalles(aD);
+		writeln('1');
+		reset(aM); reset(aD);
+		writeln('1.1');
+		while(not eof(aD)) do begin
+			read(aM,empM);
+			read(aD,empD);
+			writeln('2');
+			while(empM.nroEmpleado <> empD.nroEmpleado) do begin
+				read(aM,empM);
+			end;
+			empM.edad:= empD.edad;
+			seek(aM, filepos(aM) - 1);
+			write(aM, empM);
+			{Confío en la condición de que estén ordenados bajo el mismo criterio. SIno podría volver acá al principio del maestro o implementar una búsqueda más eficiente}	
+		end;
+	end;
+
+
+{---------------------------- B a texto --------------------------}
+
+procedure ExportarBinarioATexto(var archivoLogico:archivo; var archivoTexto: Text);
+	var
+		e: empleado;
+	begin
+		assign(archivoTexto, 'todos_empleados.txt');
+		rewrite(archivoTexto);
+		reset(archivoLogico);
+		while(not eof(archivoLogico)) do begin
+			read(archivoLogico, e);
+			writeln(archivoTexto, e.apellido, ' ', e.nombre, ' ', e.nroEmpleado, ' ', e.edad, ' ', e.dni, ' ');
+		end;
+		close(archivoLogico);
+		close(archivoTexto);
+		
+	end;
+
+procedure ExportarDNIFaltante(var archivoLogico:archivo; var archivoTextoDNI: Text);
+	var
+		e:empleado;
+	begin
+
+		reset(archivoLogico);
+		assign(archivoTextoDNI, 'faltaDNIEmpleado.txt');
+		rewrite(archivoTextoDNI);
+		while (not eof(archivoLogico)) do begin
+			read(archivoLogico,e);
+			if (e.dni = 0) then begin {elegí al inicio del ejercicio usar INT para dni, pero debería ser un string}
+				writeln(archivoTextoDNI,e.nombre, ' ', e.apellido, ' ', e.nroEmpleado, ' ', e.edad, ' ', e.dni);
+			end;
+		
+		end;
+		close(archivoTextoDNI);
+		close(archivoLogico);
+	end;
+
+
+Var
+
+	archivoNombre1: cadena20;
+	archivoLogico: archivo;
+	archivoTexto, archivoTextoDNI: Text;
+	opcion: string[1];
+	
+Begin
+	write('Ingrese el nombre del archivo con el que desea trabajar: ');
+	readln(archivoNombre1);
+	assign(archivoLogico,archivoNombre1);
+	
+	writeln('¿Qué acción desea realizar a continuación?');
+	writeln('Ingrese "a" para crear un nuevo archivo y cargar sus datos.');
+	writeln('Ingrese "b" para consultar los registros del archivo.');
+	writeln('Ingrese "c" para agregar uno o más empleados al final del archivo.');
+	writeln('Ingrese "d" para actualizar la edad de uno o más empleados.');
+	writeln('Ingrese "e" para exportar el contenido del archivo de empleados a un archivo de texto.');
+	writeln('Ingrese "f" para exportar los empleados que carecen de DNI a un archivo de texto.');
+	writeln('Para salir ingrese 0.');
+	write('Opción: ');
+	readln(opcion);
+	
+	while (opcion <> '0') do begin
+	
+		case opcion of
+			'a': CrearArchivo(archivoLogico);
+			'b': begin
+					writeln;
+					writeln('--------Consulta de registros de archivo --------');
+					writeln('Ingrese "1" para Listar en pantalla los datos de empleados que tengan un nombre o apellido determinado.');
+					writeln('Ingrese "2" para listar los datos de los empleados por línea.');
+					writeln('Ingrese "3" para listar en pantalla los empleados mayores de 70 años próximos a jubilarse.');
+					writeln('Ingrese cualquier otro caracter para salir.');
+					write('Opción: ');
+					readln(opcion);
+					case opcion of
+						'1': ImprimirNombreAp(archivoLogico);
+						'2': ImprimirTodo(archivoLogico);
+						'3': ImprimirMayores(archivoLogico);
+						else writeln('Saliendo.');
+					end;
+			
+				 end;
+			'c': AgregarAlFinal(archivoLogico);
+			'd': ActualizarMaestroDetalle(archivoLogico);
+			'e': ExportarBinarioATexto(archivoLogico,archivoTexto);
+			'f': ExportarDNIFaltante(archivoLogico,archivoTextoDNI);
+			else
+				writeln('Opción incorrecta. Por favor, ingrese una opción válida.');
+			end;
+			
+		writeln('--------------------------------------');
+		writeln('¿Qué acción desea realizar a continuación?');
+		writeln('Ingrese "a" para crear un nuevo archivo y cargar sus datos.');
+		writeln('Ingrese "b" para consultar los registros del archivo.');
+		writeln('Ingrese "c" para agregar uno o más empleados al final del archivo.');
+		writeln('Ingrese "d" para actualizar la edad de uno o más empleados.');
+		writeln('INgrese "e" para exportar el contenido del archivo de empleados a un archivo de texto.');
+		writeln('Ingrese "f" para exportar los empleados que carecen de DNI a un archivo de texto.');
+		writeln('Para salir ingrese 0.');
+		write('Opción: ');
+		readln(opcion);	
+	end;
+
+	writeln('FIN DEL PROGRAMA');
+	
+End.
---
+++ 1/ej05/p01ej05.pas
@@ -0,0 +1,159 @@
+{5. Realizar un programa para una tienda de celulares, que presente un menú con
+opciones para:
+a.
+Crear un archivo de registros no ordenados de celulares y cargarlo con datos
+ingresados desde un archivo de texto denominado “celulares.txt”. Los registros
+correspondientes a los celulares, deben contener: código de celular, el nombre,
+descripcion, marca, precio, stock mínimo y el stock disponible.
+b.
+Listar en pantalla los datos de aquellos celulares que tengan un stock menor al
+stock mínimo.
+c.
+Listar en pantalla los celulares del archivo cuya descripción contenga una
+cadena de caracteres proporcionada por el usuario.
+d.
+Exportar el archivo creado en el inciso a) a un archivo de texto denominado
+“celulares.txt” con todos los celulares del mismo.
+NOTA 1: El nombre del archivo binario de celulares debe ser proporcionado por el usuario
+una única vez.
+NOTA 2: El archivo de carga debe editarse de manera que cada celular se especifique en
+tres líneas consecutivas: en la primera se especifica: código de celular, el precio y
+marca, en la segunda el stock disponible, stock mínimo y la descripción y en la tercera
+nombre en ese orden. Cada celular se carga leyendo tres líneas del archivo
+“celulares.txt”.}
+
+
+{1. se carga desde celulares.txt
+*2. procesar listando los del stock tuki
+*3. mostrar lo especificado
+*4. exportar nuevamente el archivo}
+
+Type
+	cadena15 = string[15];
+	cadena100 = string[100];
+	celular = record
+		cod: integer;
+		nombre: cadena15;
+		descripcion: cadena100;
+		marca: cadena15;
+		precio: real;
+		stockMin: integer;
+		stock: integer;
+	end;
+	
+	arch = file of celular;
+
+procedure ImprimirCelular(c:celular);
+	begin
+		writeln('--------Celular: ', c.nombre);
+		writeln('->Código: ', c.cod);
+		writeln('->Descripción: ', c.descripcion);
+		writeln('->Marca: ', c.marca);
+		writeln('->precio: ', c.precio);
+		writeln('->stock mínimo: ', c.stockMin);
+		writeln('->stock actual: ', c.stock);
+	end;
+
+procedure CrearArchBinario(var aLB: arch; var archTxt: Text);
+	var
+		c: celular;
+	begin
+		reset(archTxt);
+		rewrite(aLB);
+		while(not eof(archTxt)) do begin
+			{voy leyendo y almacenando en los campos de "c". Tener en cuenta que los espacios separa por defecto, creo.}
+			readln(archTxt, c.nombre, c.cod, c.precio, c.marca); {acrego el nombre porque no figura en la consigna}
+			readln(archTxt, c.stockMin, c.stock);
+			readln(archTxt, c.descripcion);
+			write(aLB,c);
+		end;
+		close(aLB);
+		close(archTxt);
+		
+		writeln('----------ARCHIVO BINARIO CREADO Y CARGADO -------------');
+	end;
+
+
+
+
+procedure ListarBajoStock(var aLB: arch);
+	var
+		c:celular;
+	begin
+		reset(aLB);
+		while(not eof(aLB)) do begin
+			read(aLB,c);
+			if (c.stock < c.stockMin) then
+				ImprimirCelular(c);
+		end;
+		close(aLB);
+		
+	end;
+	
+procedure ListarConDesc(var aLB: arch);
+	var
+		c:celular;
+	begin
+		reset(aLB);
+		while(not eof(aLB)) do begin
+			read(aLB,c);
+			if (c.descripcion <> '') then
+				ImprimirCelular(c);
+		end;
+		close(aLB);
+	end;
+
+procedure SobreescribirArchTxt(var archLogBin:arch; var archTexto:Text);
+	var
+		c:celular;
+	begin
+		reset(archLogBin);
+		rewrite(archTexto);
+		while(not eof(archLogBin)) do begin
+			read(archLogBin,c);
+			writeln(archTexto, c.nombre, ' ', c.cod,' ',c.precio, ' ', c.marca);
+			writeln(archTexto, c.stockMin, ' ', c.stock);
+			writeln(archTexto, c.descripcion);
+		end;
+	
+	end;
+
+Var
+	archLogBin: arch;
+	archTexto: Text;
+	nombreBin: cadena15;
+	opcion: char;
+Begin
+	writeln('Ingresá el nombre del archivo con el que querés trabajar. Tenés 15 caracteres.');
+	readln(nombreBin);
+	assign(archLogBin,nombreBin);
+	assign(archTexto,'celulares.txt');
+	
+	writeln;
+	writeln('¿Que deseas hacer a continuación?');
+	writeln('a. Crear archivo binario y cargarlo con archivo de texto ("celulares.txt").');
+	writeln('b. Listar en pantalla aquellos celulares que tienen un stock inferior al mínimo.');
+	writeln('c. Listar en pantalla aquellos celulares que poseen descripción.');
+	writeln('d. Volver a escribir el archivo "celulares.txt" con el contenido del archivo binario generado.');
+	writeln('Para salir, ingrese "0".');
+	readln(opcion);
+	
+	while (opcion <> '0') do begin
+		case opcion of 
+			'a': CrearArchBinario(archLogBin,archTexto);
+			'b': ListarBajoStock(archLogBIn);
+			'c': ListarConDesc(archLogBin);
+			'd': SobreescribirArchTxt(archLogBin,archTexto);
+			else
+				writeln('Opción incorrecta, ingrese una opción válida');
+		end;
+		writeln;
+		writeln('¿Que deseas hacer a continuación?');
+		writeln('a. Crear archivo binario y cargarlo con archivo de texto ("celulares.txt").');
+		writeln('b. Listar en pantalla aquellos celulares que tienen un stock inferior al mínimo.');
+		writeln('c. Listar en pantalla aquellos celulares que poseen descripción.');
+		writeln('d. Volver a escribir el archivo "celulares.txt" con el contenido del archivo binario generado.');
+		writeln('Para salir, ingrese "0".');
+		readln(opcion);
+	end;
+End.
